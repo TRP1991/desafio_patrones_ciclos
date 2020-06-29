461beac22067d37b4b3b196f933ab717281d5bff
Tamara Rubilar <ta.rubilar@gmail.com>
2020-06-29 19:12:23 -0400

    adjustments in Pattern 2. Start of Pattern 4 program

--- patron2.rb
+++ patron2.rb
@@ -1,4 +1,4 @@
-n = ARGV[0].to_i # 24
+n = ARGV[0].to_i 
 n.times do |i|
     if i%4 == 0 || i%4 == 1
         print '*'
---
+++ patron4.rb
@@ -0,0 +1,10 @@
+n = ARGV[0].to_i 
+n.times do |i|
+    if i.even?
+        print '1'
+    elsif
+        print '2'
+    else
+        print "3"
+    end
+end
